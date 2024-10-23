1038719138f29ada4e1bdf8b67150acba541e03c
TrinityCactus <166144814+TrinityCactus@users.noreply.github.com>
2024-10-23 18:29:26 +0200

    Bearbeiten der Searchphase
    Erstellen der Buttons für die Türen.
    
    Bild für Direkt zur Buildphase

--- Assets/Tests/Johanna/search_phase.gd
+++ Assets/Tests/Johanna/search_phase.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print(get_parent().find_child("build_phase"))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_button_bild_pressed():
+	get_tree().change_scene_to_file("res://Assets/Tests/Joran/build_phase.tscn")
