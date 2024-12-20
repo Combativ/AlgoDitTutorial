408c868faf93c39142621ce86aa3c651004eef3a
Joran <joran@zurmuehls.de>
2024-12-20 23:54:45 +0100

    Added title screen and renamed game

--- menus/title/title.gd
+++ menus/title/title.gd
@@ -2,11 +2,10 @@ extends Node2D
 
 @onready var play_button: Button = $PlayButton
 @onready var quit_button: Button = $QuitButton
-@onready var main: Main = $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play_button.pressed.connect(main.switch_to_level_01)
+	play_button.pressed.connect(_on_play_button_pressed)
 	quit_button.pressed.connect(func(): get_tree().quit())
 	pass # Replace with function body.
 
@@ -14,3 +13,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
